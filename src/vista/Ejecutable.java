a95bc8494b4863da47ed8f07dca0a1f223be34b9
stban2703 <s.t.ban1401@gmail.com>
2019-11-29 12:33:49 -0500

    first commit

---
+++ src/vista/Ejecutable.java
@@ -0,0 +1,28 @@
+package vista;
+
+import processing.core.PApplet;
+
+public class Ejecutable extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main(Ejecutable.class.getName());
+
+	}
+	
+	public void settings() {
+		
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void mousePressed() {
+		
+	}
+
+}
